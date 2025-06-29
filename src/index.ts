3af9ed9300705a41a9c50e34a64705b405f66ccc
Priapisman677 <expeliamm02@hotmail.com>
2025-06-29 08:39:48 -0700

    u

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,6 @@
 export * from './rate-limiter.js'
 export * from './types.js'
+
+//* In case you need independent tests:
+export * from './utils/get-full-redis-key.js'
+export * from './utils/get-request-ip.js'
